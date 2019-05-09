bf8512deca7b86b7609a5471ea3b8623c07ccae7
Perrin Antonin <tom.per@orange.fr>
2019-05-09 22:50:11 +0200

    test commit from php storm

--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -9,6 +9,16 @@
                 <form enctype="multipart/form-data" action="{{ route('profile.update') }}" method="POST">
                     @method('PATCH')
                     @csrf
+
+                    <img src="/uploads/banners/{{ $user->avatar }}" style="width:900px; height:300px; float:left;">
+                    <label>Update Profile Image</label>
+                    <input type="file" name="avatar">
+
+                    <img src="/uploads/avatars/{{ $user->avatar }}" style="width:150px; height:150px; float:left; border-radius:50%; margin-right:25px;">
+                    <label>Update Profile Image</label>
+                    <input type="file" name="avatar">
+
+
                     <h1 class="title" style="margin-top: 2em;">Edit name</h1>
                     <div class="field">
                         <label class="label" for="name">Username</label>
@@ -17,10 +27,22 @@
                         </div>
                     </div>
 
+                    <h1 class="title" style="margin-top: 2em;">Edit City</h1>
+                    <div class="field">
+                        <label class="label" for="city">City</label>
+                        <div class="control">
+                            <input type="text" class="input" name="city" required placeholder="Message" value="{{$user->city}}">
+                        </div>
+                    </div>
 
-                    <img src="/uploads/avatars/{{ $user->avatar }}" style="width:150px; height:150px; float:left; border-radius:50%; margin-right:25px;">
-                    <label>Update Profile Image</label>
-                    <input type="file" name="avatar">
+                    <h1 class="title" style="margin-top: 2em;">Edit Link</h1>
+                    <div class="field">
+                        <label class="label" for="link">Link</label>
+                        <div class="control">
+                            <input type="text" class="input" name="link" required placeholder="Message" value="{{$user->link}}">
+                        </div>
+                    </div>
+
                     <button type ="submit" class="button is-link">Update profile</button>
                 </form>
             </div>
