741c0d6ab31ecb974b16b36498158868186bcfeb
Pierrelippe <pie15400@gmail.com>
2019-06-16 19:14:31 +0200

    +red button

--- resources/views/user/userProfile.blade.php
+++ resources/views/user/userProfile.blade.php
@@ -146,7 +146,7 @@
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary" id="modal-save">Save changes</button>
+                        <button type="button" class="btn btn-light btnstyle-1" id="modal-save">Save changes</button>
                     </div>
                 </div><!-- /.modal-content -->
             </div><!-- /.modal-dialog -->
