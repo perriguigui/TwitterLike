ab9f4f314009a814fad4148e9417fc206b9f7673
Pierrelippe <pie15400@gmail.com>
2019-06-16 17:53:56 +0200

    +rtcss

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -57,14 +57,20 @@
                                                 <p>faut se connecter pour liker</p>
                                             @endif
                                             <p class="d-inline color_rouge ml-2">{{$post->retweetsCount->count()}}</p>
-                                            <a href="{{ route('retweet', ['user_id' => Auth::user()->id,'post_id' => $post->id]) }}" class="color_rouge">Retweet</a>
+                                            <div class="dropdown">
+                                                <a href="{{ route('retweet', ['user_id' => Auth::user()->id,'post_id' => $post->id]) }}" class="color_rouge">Retweet</a>
+                                                <div class="dropdown-content">
+                                                    <p style="color: #E11531;">Retweet by :</p>
+                                                    @foreach ($post->retweets as $object)
+                                                        <p class="retweet_name-style "> {{ $object->user->name }}</p>
+                                                    @endforeach
+                                                </div>
+                                            </div>
                                             @if(Auth::user()==$post->user)
                                                 <a href="#" class="edit color_rouge ml-4">Edit</a>
                                                 <a href="{{ route('post.delete', ['post_id' => $post->id]) }}" class="color_rouge">Delete</a>
                                             @endif
-                                            @foreach ($post->retweets as $object)
-                                                <p class="d-inline retweet_name-style "> Retweet by {{ $object->user->name }}</p>
-                                            @endforeach
+
 
                                         </div>
                                     </article>
